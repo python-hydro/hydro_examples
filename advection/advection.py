4ebd940dd751cb412baad2ca6967f6866163c89f
Michael Zingale <michael.zingale@stonybrook.edu>
2014-06-21 21:24:39 -0400

    some restructuring

--- advection/advection.py
+++ advection/advection.py
@@ -9,220 +9,6 @@ import numpy
 import pylab
 import math
 
-class ccFVgrid:
-
-    def __init__(self, nx, ng, xmin=0.0, xmax=1.0):
-
-        self.xmin = xmin
-        self.xmax = xmax
-        self.ng = ng
-        self.nx = nx
-
-        # python is zero-based.  Make easy intergers to know where the
-        # real data lives
-        self.ilo = ng
-        self.ihi = ng+nx-1
-
-        # physical coords -- cell-centered, left and right edges
-        self.dx = (xmax - xmin)/(nx)
-        self.x = xmin + (numpy.arange(nx+2*ng)-ng+0.5)*self.dx
-        self.xl = xmin + (numpy.arange(nx+2*ng)-ng)*self.dx
-        self.xr = xmin + (numpy.arange(nx+2*ng)-ng+1.0)*self.dx
-
-        # storage for the solution
-        self.a = numpy.zeros((nx+2*ng), dtype=numpy.float64)
-
-    def period(self, u):
-        """ return the period for advection with velocity u """
-        return (self.xmax - self.xmin)/u
-
-    def scratch_array(self):
-        """ return a scratch array dimensioned for our grid """
-        return numpy.zeros((self.nx+2*self.ng), dtype=numpy.float64)
-
-    def fill_bc(self):
-        """ fill all single ghostcell with periodic boundary conditions """
-
-        # left boundary
-        n = 0
-        while (n < self.ng):
-            self.a[self.ilo-1-n] = self.a[self.ihi-n]
-            n += 1
-
-        # right boundary
-        n = 0
-        while (n < self.ng):
-            self.a[self.ihi+1+n] = self.a[self.ilo+n]
-            n += 1
-
-    def init_cond(self, type="tophat"):
-
-        if type == "tophat":
-            self.a[numpy.logical_and(self.x >= 0.333, self.x <= 0.666)] = 1.0
-        elif type == "sine":
-            self.a[:] = numpy.sin(2.0*math.pi*self.x/(self.xmax-self.xmin))
-        elif type == "gaussian":
-            self.a[:] = 1.0 + numpy.exp(-60.0*(self.x - 0.5)**2)
-
-        self.ainit = self.a.copy()
-
-    def norm(self, e):
-        """ return the norm of quantity e which lives on the grid """
-        if not len(e) == (2*self.ng + self.nx):
-            return None
-
-        return numpy.sqrt(self.dx*numpy.sum(e[self.ilo:self.ihi+1]**2))
-
-
-
-#-----------------------------------------------------------------------------
-# advection-specific routines
-
-def timestep(g, C, u):
-    return C*g.dx/u
-
-
-def states(g, dt, u, slope_type):
-    """ compute the left and right interface states """
-
-    # compute the piecewise linear slopes
-    slope = g.scratch_array()
-
-
-    if slope_type == "godunov":
-
-        # piecewise constant = 0 slopes
-        slope[:] = 0.0
-
-    elif slope_type == "centered":
-
-        # unlimited centered difference slopes
-
-        i = g.ilo-1
-        while (i <= g.ihi+1):
-            slope[i] = 0.5*(g.a[i+1] - g.a[i-1])/g.dx
-            i += 1
-
-    elif slope_type == "minmod":
-
-        # minmod limited slope
-
-        i = g.ilo-1
-        while (i <= g.ihi+1):
-            slope[i] = minmod( (g.a[i] - g.a[i-1])/g.dx, 
-                               (g.a[i+1] - g.a[i])/g.dx )
-            i += 1
-        
-    elif slope_type == "MC":
-
-        # MC limiter
-
-        i = g.ilo-1
-        while (i <= g.ihi+1):
-            slope[i] = minmod(minmod( 2.0*(g.a[i] - g.a[i-1])/g.dx, 
-                                      2.0*(g.a[i+1] - g.a[i])/g.dx ),
-                              0.5*(g.a[i+1] - g.a[i-1])/g.dx)
-            i += 1
-
-    elif slope_type == "superbee":
-
-        # superbee limiter
-
-        i = g.ilo-1
-        while (i <= g.ihi+1):
-            A = minmod( (g.a[i+1] - g.a[i])/g.dx,
-                        2.0*(g.a[i] - g.a[i-1])/g.dx )
-
-            B = minmod( (g.a[i] - g.a[i-1])/g.dx,
-                        2.0*(g.a[i+1] - g.a[i])/g.dx )
-            
-            slope[i] = maxmod(A, B)
-            i += 1
-
-
-
-    # loop over all the interfaces.  Here, i refers to the left
-    # interface of the zone.  Note that thre are 1 more interfaces
-    # than zones
-    al = g.scratch_array()
-    ar = g.scratch_array()
-
-    i = g.ilo
-    while (i <= g.ihi+1):
-
-        # left state on the current interface comes from zone i-1
-        al[i] = g.a[i-1] + 0.5*g.dx*(1.0 - u*dt/g.dx)*slope[i-1]
-
-        # right state on the current interface comes from zone i
-        ar[i] = g.a[i] - 0.5*g.dx*(1.0 + u*dt/g.dx)*slope[i]
-
-        i += 1
-
-    return al, ar
-
-
-def riemann(u, al, ar):
-    """ Riemann problem for advection -- this is simply upwinding,
-        but we return the flux """
-
-    if u > 0.0:
-        return u*al
-    else:
-        return u*ar
-
-
-def update(g, dt, flux):
-    """ conservative update """
-
-    anew = g.scratch_array()
-
-    anew[g.ilo:g.ihi+1] = g.a[g.ilo:g.ihi+1] + \
-        dt/g.dx * (flux[g.ilo:g.ihi+1] - flux[g.ilo+1:g.ihi+2])
-
-    return anew
-
-
-def evolve(nx, C, u, numPeriods, problem_name, slope_type="centered"):
-
-    ng = 2
-
-    # create the grid
-    g = ccFVgrid(nx, ng)
-
-    t = 0.0
-    tmax = numPeriods*g.period(u)
-
-    # initialize the data
-    g.init_cond(problem_name)
-
-
-    # main evolution loop
-    while (t < tmax):
-
-        # fill the boundary conditions
-        g.fill_bc()
-
-        # get the timestep
-        dt = timestep(g, C, u)
-
-        if (t + dt > tmax):
-            dt = tmax - t
-
-        # get the interface states
-        al, ar = states(g, dt, u, slope_type)
-
-        # solve the Riemann problem at all interfaces
-        flux = riemann(u, al, ar)
-        
-        # do the conservative update
-        anew = update(g, dt, flux)
-
-        g.a[:] = anew[:]
-
-        t += dt
-
-    return g
-
 
 def minmod(a, b):
     if (abs(a) < abs(b) and a*b > 0.0):
@@ -241,18 +27,245 @@ def maxmod(a, b):
         return 0.0
 
 
+class Grid1d:
+
+    def __init__(self, nx, ng, xmin=0.0, xmax=1.0):
+
+        self.ng = ng
+        self.nx = nx
+
+        self.xmin = xmin
+        self.xmax = xmax
+
+        # python is zero-based.  Make easy intergers to know where the
+        # real data lives
+        self.ilo = ng
+        self.ihi = ng+nx-1
+
+        # physical coords -- cell-centered, left and right edges
+        self.dx = (xmax - xmin)/(nx)
+        self.x = xmin + (numpy.arange(nx+2*ng)-ng+0.5)*self.dx
+
+        # storage for the solution
+        self.a = numpy.zeros((nx+2*ng), dtype=numpy.float64)
+
+
+    def scratch_array(self):
+        """ return a scratch array dimensioned for our grid """
+        return numpy.zeros((self.nx+2*self.ng), dtype=numpy.float64)
+
+
+    def fill_BCs(self):
+        """ fill all single ghostcell with periodic boundary conditions """
+
+        # left boundary
+        n = 0
+        while n < self.ng:
+            self.a[self.ilo-1-n] = self.a[self.ihi-n]
+            n += 1
+
+        # right boundary
+        n = 0
+        while n < self.ng:
+            self.a[self.ihi+1+n] = self.a[self.ilo+n]
+            n += 1
+
+
+    def norm(self, e):
+        """ return the norm of quantity e which lives on the grid """
+        if not len(e) == (2*self.ng + self.nx):
+            return None
+
+        return numpy.sqrt(self.dx*numpy.sum(e[self.ilo:self.ihi+1]**2))
+
+
+
+class Simulation:
+
+    def __init__(self, grid, u, slope_type="centered"):
+        self.grid = grid
+        self.t = 0.0
+        self.u = u   # the constant advective velocity
+        self.slope_type = slope_type
+
+
+    def init_cond(self, type="tophat"):
+
+        if type == "tophat":
+            self.grid.a[numpy.logical_and(self.grid.x >= 0.333, 
+                                          self.grid.x <= 0.666)] = 1.0
+
+        elif type == "sine":
+            self.grid.a[:] = numpy.sin(2.0*math.pi*self.grid.x/(self.grid.xmax-self.grid.xmin))
+
+        elif type == "gaussian":
+            self.grid.a[:] = 1.0 + numpy.exp(-60.0*(self.grid.x - 0.5)**2)
+
+
+    def timestep(self, C):
+        return C*self.grid.dx/self.u
+
+
+    def period(self):
+        """ return the period for advection with velocity u """
+        return (self.grid.xmax - self.grid.xmin)/self.u
+
+
+    def states(self, dt):
+        """ compute the left and right interface states """
+
+        # compute the piecewise linear slopes
+        g = self.grid
+        slope = g.scratch_array()
+
+        g = self.grid
+        
+        if self.slope_type == "godunov":
+
+            # piecewise constant = 0 slopes
+            slope[:] = 0.0
+
+        elif self.slope_type == "centered":
+
+            # unlimited centered difference slopes
+            i = g.ilo-1
+            while i <= g.ihi+1:
+                slope[i] = 0.5*(g.a[i+1] - g.a[i-1])/g.dx
+                i += 1
+
+        elif self.slope_type == "minmod":
+
+            # minmod limited slope
+            i = g.ilo-1
+            while i <= g.ihi+1:
+                slope[i] = minmod( (g.a[i] - g.a[i-1])/g.dx, 
+                                   (g.a[i+1] - g.a[i])/g.dx )
+                i += 1
+        
+        elif self.slope_type == "MC":
+
+            # MC limiter
+            i = g.ilo-1
+            while i <= g.ihi+1:
+                slope[i] = minmod(minmod( 2.0*(g.a[i] - g.a[i-1])/g.dx, 
+                                          2.0*(g.a[i+1] - g.a[i])/g.dx ),
+                                  0.5*(g.a[i+1] - g.a[i-1])/g.dx)
+                i += 1
+
+        elif self.slope_type == "superbee":
+
+            # superbee limiter
+            i = g.ilo-1
+            while i <= g.ihi+1:
+                A = minmod( (g.a[i+1] - g.a[i])/g.dx,
+                            2.0*(g.a[i] - g.a[i-1])/g.dx )
+
+                B = minmod( (g.a[i] - g.a[i-1])/g.dx,
+                            2.0*(g.a[i+1] - g.a[i])/g.dx )
+            
+                slope[i] = maxmod(A, B)
+                i += 1
+
+
+
+        # loop over all the interfaces.  Here, i refers to the left
+        # interface of the zone.  Note that thre are 1 more interfaces
+        # than zones
+        al = g.scratch_array()
+        ar = g.scratch_array()
+
+        i = g.ilo
+        while i <= g.ihi+1:
+
+            # left state on the current interface comes from zone i-1
+            al[i] = g.a[i-1] + 0.5*g.dx*(1.0 - u*dt/g.dx)*slope[i-1]
+
+            # right state on the current interface comes from zone i
+            ar[i] = g.a[i] - 0.5*g.dx*(1.0 + u*dt/g.dx)*slope[i]
+
+            i += 1
+
+        return al, ar
+
+
+    def riemann(self, al, ar):
+        """ 
+        Riemann problem for advection -- this is simply upwinding,
+        but we return the flux 
+        """
+
+        if self.u > 0.0:
+            return self.u*al
+        else:
+            return self.u*ar
+
+
+    def update(self, dt, flux):
+        """ conservative update """
+
+        g = self.grid
+
+        anew = g.scratch_array()
+
+        anew[g.ilo:g.ihi+1] = g.a[g.ilo:g.ihi+1] + \
+            dt/g.dx * (flux[g.ilo:g.ihi+1] - flux[g.ilo+1:g.ihi+2])
+
+        return anew
+
+
+    def evolve(self, C, num_periods=1):
+
+        self.t = 0.0
+        g = self.grid
+
+        tmax = num_periods*self.period()
+
+
+        # main evolution loop
+        while (self.t < tmax):
+
+            # fill the boundary conditions
+            g.fill_BCs()
+
+            # get the timestep
+            dt = self.timestep(C)
+
+            if (self.t + dt > tmax):
+                dt = tmax - self.t
+
+            # get the interface states
+            al, ar = self.states(dt)
+
+            # solve the Riemann problem at all interfaces
+            flux = self.riemann(al, ar)
+        
+            # do the conservative update
+            anew = self.update(dt, flux)
+
+            g.a[:] = anew[:]
+
+            self.t += dt
+
 
 #-----------------------------------------------------------------------------
 # tophat
 
-u = 1.0
+xmin = 0.0
+xmax = 1.0
 nx = 64
-C = 0.8
+ng = 2
+g = Grid1d(nx, ng, xmin=xmin, xmax=xmax)
 
-g = evolve(nx, C, u, 5, "tophat")
+u = 1.0
+s = Simulation(g, u, slope_type="minmod")
+s.init_cond("tophat")
+ainit = s.grid.a.copy()
+
+C = 0.8
+s.evolve(C, num_periods=5)
 
 pylab.plot(g.x[g.ilo:g.ihi+1], g.a[g.ilo:g.ihi+1], color="r")
-pylab.plot(g.x[g.ilo:g.ihi+1], g.ainit[g.ilo:g.ihi+1], ls=":", color="0.5")
+pylab.plot(g.x[g.ilo:g.ihi+1], ainit[g.ilo:g.ihi+1], ls=":", color="0.5")
 
 pylab.savefig("fv-advect-tophat.png")
 
@@ -260,16 +273,24 @@ pylab.savefig("fv-advect-tophat.png")
 #-----------------------------------------------------------------------------
 # gaussian
 
-u = 1.0
+xmin = 0.0
+xmax = 1.0
 nx = 64
-C = 0.8
+ng = 2
+g = Grid1d(nx, ng, xmin=xmin, xmax=xmax)
 
-g = evolve(nx, C, u, 5, "gaussian")
+u = 1.0
+s = Simulation(g, u, slope_type="minmod")
+s.init_cond("gaussian")
+ainit = s.grid.a.copy()
+
+C = 0.8
+s.evolve(C, num_periods=5)
 
 pylab.clf()
 
 pylab.plot(g.x[g.ilo:g.ihi+1], g.a[g.ilo:g.ihi+1], color="r")
-pylab.plot(g.x[g.ilo:g.ihi+1], g.ainit[g.ilo:g.ihi+1], ls=":", color="0.5")
+pylab.plot(g.x[g.ilo:g.ihi+1], ainit[g.ilo:g.ihi+1], ls=":", color="0.5")
 
 pylab.savefig("fv-advect-gaussian.png")
 
@@ -278,18 +299,28 @@ pylab.savefig("fv-advect-gaussian.png")
 #-----------------------------------------------------------------------------
 # convergence test
 problem = "gaussian"
+
+xmin = 0.0
+xmax = 1.0
+ng = 2
 N = [32, 64, 128, 256]
-u = 1.0
-C = 0.8
 
 err = []
 
 for nx in N:
 
-    g = evolve(nx, C, u, 5, problem)
+    g = Grid1d(nx, ng, xmin=xmin, xmax=xmax)
+
+    u = 1.0
+    s = Simulation(g, u, slope_type="centered")
+    s.init_cond("gaussian")
+    ainit = s.grid.a.copy()
+
+    C = 0.8
+    s.evolve(C, num_periods=5)
 
     # compute the error
-    err.append(g.norm(g.a - g.ainit))
+    err.append(g.norm(g.a - ainit))
     print g.dx, nx, err[-1]
 
 
@@ -316,83 +347,92 @@ pylab.savefig("plm-converge.png")
 #-----------------------------------------------------------------------------
 # gaussian -- different limiters
 
-u = 1.0
+xmin = 0.0
+xmax = 1.0
 nx = 128
+ng = 2
+
 C = 0.8
+u = 1.0
 
-g = evolve(nx, C, u, 5, "gaussian", slope_type="godunov")
+g= Grid1d(nx, ng, xmin=xmin, xmax=xmax)
 
-pylab.clf()
+s = Simulation(g, u, slope_type="godunov")
+s.init_cond("gaussian")
+ainit = s.grid.a.copy()
+
+s.evolve(C, num_periods=5)
+
+pylab.subplot(231)
 
 pylab.plot(g.x[g.ilo:g.ihi+1], g.a[g.ilo:g.ihi+1], color="r")
-pylab.plot(g.x[g.ilo:g.ihi+1], g.ainit[g.ilo:g.ihi+1], ls=":", color="0.5")
+pylab.plot(g.x[g.ilo:g.ihi+1], ainit[g.ilo:g.ihi+1], ls=":", color="0.5")
 
 pylab.title("piecewise constant")
 
-f = pylab.gcf()
-f.set_size_inches(6.0,7.0)
 
-pylab.savefig("fv-gaussian-constant.png", bbox_inches="tight")
+s = Simulation(g, u, slope_type="centered")
+s.init_cond("gaussian")
+ainit = s.grid.a.copy()
 
+s.evolve(C, num_periods=5)
 
-g = evolve(nx, C, u, 5, "gaussian", slope_type="centered")
-
-pylab.clf()
+pylab.subplot(232)
 
 pylab.plot(g.x[g.ilo:g.ihi+1], g.a[g.ilo:g.ihi+1], color="r")
-pylab.plot(g.x[g.ilo:g.ihi+1], g.ainit[g.ilo:g.ihi+1], ls=":", color="0.5")
+pylab.plot(g.x[g.ilo:g.ihi+1], ainit[g.ilo:g.ihi+1], ls=":", color="0.5")
 
 pylab.title("centered (unlimited)")
 
-f = pylab.gcf()
-f.set_size_inches(6.0,7.0)
 
-pylab.savefig("fv-gaussian-centered.png", bbox_inches="tight")
+s = Simulation(g, u, slope_type="minmod")
+s.init_cond("gaussian")
+ainit = s.grid.a.copy()
 
+s.evolve(C, num_periods=5)
 
-g = evolve(nx, C, u, 5, "gaussian", slope_type="minmod")
-
-pylab.clf()
+pylab.subplot(233)
 
 pylab.plot(g.x[g.ilo:g.ihi+1], g.a[g.ilo:g.ihi+1], color="r")
-pylab.plot(g.x[g.ilo:g.ihi+1], g.ainit[g.ilo:g.ihi+1], ls=":", color="0.5")
+pylab.plot(g.x[g.ilo:g.ihi+1], ainit[g.ilo:g.ihi+1], ls=":", color="0.5")
 
 pylab.title("minmod limiter")
 
-f = pylab.gcf()
-f.set_size_inches(6.0,7.0)
 
-pylab.savefig("fv-gaussian-minmod.png", bbox_inches="tight")
+s = Simulation(g, u, slope_type="MC")
+s.init_cond("gaussian")
+ainit = s.grid.a.copy()
 
+s.evolve(C, num_periods=5)
 
-g = evolve(nx, C, u, 5, "gaussian", slope_type="MC")
-
-pylab.clf()
+pylab.subplot(234)
 
 pylab.plot(g.x[g.ilo:g.ihi+1], g.a[g.ilo:g.ihi+1], color="r")
-pylab.plot(g.x[g.ilo:g.ihi+1], g.ainit[g.ilo:g.ihi+1], ls=":", color="0.5")
+pylab.plot(g.x[g.ilo:g.ihi+1], ainit[g.ilo:g.ihi+1], ls=":", color="0.5")
 
 pylab.title("MC limiter")
 
-f = pylab.gcf()
-f.set_size_inches(6.0,7.0)
 
-pylab.savefig("fv-gaussian-MC.png", bbox_inches="tight")
+s = Simulation(g, u, slope_type="superbee")
+s.init_cond("gaussian")
+ainit = s.grid.a.copy()
 
+s.evolve(C, num_periods=5)
 
-g = evolve(nx, C, u, 5, "gaussian", slope_type="superbee")
-
-pylab.clf()
+pylab.subplot(235)
 
 pylab.plot(g.x[g.ilo:g.ihi+1], g.a[g.ilo:g.ihi+1], color="r")
-pylab.plot(g.x[g.ilo:g.ihi+1], g.ainit[g.ilo:g.ihi+1], ls=":", color="0.5")
+pylab.plot(g.x[g.ilo:g.ihi+1], ainit[g.ilo:g.ihi+1], ls=":", color="0.5")
 
 pylab.title("superbee limiter")
 
-f = pylab.gcf()
-f.set_size_inches(6.0,7.0)
 
-pylab.savefig("fv-gaussian-superbee.png", bbox_inches="tight")
+f = pylab.gcf()
+f.set_size_inches(10.0,7.0)
+
+pylab.tight_layout()
+
+pylab.savefig("fv-gaussian-limiters.png", bbox_inches="tight")
 
 
 #-----------------------------------------------------------------------------
