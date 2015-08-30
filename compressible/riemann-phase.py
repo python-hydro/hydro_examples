c4cdfb26e6c587c70a8c9b931a8daae1810aca46
Michael Zingale <michael.zingale@stonybrook.edu>
2015-08-30 12:13:05 -0400

    this is an updated classier version that is identical to the one in numerical_exercises/figures/Euler/riemann.py

--- compressible/riemann-phase.py
+++ compressible/riemann-phase.py
@@ -1,99 +1,119 @@
 # plot the Hugoniot loci for a compressible Riemann problem
 
+import matplotlib.pyplot as plt
 import numpy as np
-import pylab
+import scipy.optimize as optimize
 
-gamma = 1.4
-
-class State:
-    """ a simple container """
+class State(object):
     def __init__(self, p=1.0, u=0.0, rho=1.0):
          self.p = p
          self.u = u
          self.rho = rho
 
+    def __str__(self):
+        return "rho: {}; u: {}; p: {}".format(self.rho, self.u, self.p)
 
-def u_hugoniot(p, state, dir):
+class RiemannProblem(object):
+    def __init__(self, left_state, right_state, gamma=1.4):
+        self.left = left_state
+        self.right = right_state
+        self.gamma = gamma
 
-    c = np.sqrt(gamma*state.p/state.rho)
+    def u_hugoniot(self, p, side):
 
-    u = np.zeros_like(p)
+        if side == "left":
+            state = self.left
+            s = 1.0
+        elif side == "right":
+            state = self.right
+            s = -1.0
 
-    if dir == "left":
-        s = 1.0
-    elif dir == "right":
-        s = -1.0
+        c = np.sqrt(self.gamma*state.p/state.rho)
 
-    # rarefaction
-    index_rare = np.where(p < state.p)
-    u[index_rare] = state.u + s*(2.0*c/(gamma-1.0))* \
-        (1.0 - (p[index_rare]/state.p)**((gamma-1.0)/(2.0*gamma)))
+        if p < state.p:
+            # rarefaction
+            u = state.u + s*(2.0*c/(self.gamma-1.0))* \
+                (1.0 - (p/state.p)**((self.gamma-1.0)/(2.0*self.gamma)))
+        else:
+            # shock
+            beta = (self.gamma+1.0)/(self.gamma-1.0)
+            u = state.u + s*(2.0*c/np.sqrt(2.0*self.gamma*(self.gamma-1.0)))* \
+                (1.0 - p/state.p)/np.sqrt(1.0 + beta*p/state.p)
 
-    # shock
-    index_shock = np.where(p >= state.p)
-    beta = (gamma+1.0)/(gamma-1.0)
-    u[index_shock] = state.u + s*(2.0*c/np.sqrt(2.0*gamma*(gamma-1.0)))* \
-        (1.0 - p[index_shock]/state.p)/np.sqrt(1.0 + beta*p[index_shock]/state.p)
+        return u
 
-    return u, index_shock, index_rare
+    def find_star_state(self, p_min=0.001, p_max=1000.0):
+        # we need to root-find on
+        pstar = optimize.brentq(lambda p: self.u_hugoniot(p, "left") - self.u_hugoniot(p, "right"),
+                               p_min, p_max)
+        ustar = self.u_hugoniot(pstar, "left")
+
+        return pstar, ustar
+
+    def plot_hugoniot(self, p_min = 0.0, p_max=1.5, N=200):
+
+        p = np.linspace(p_min, p_max, num=N)
+        u_left = np.zeros_like(p)
+        u_right = np.zeros_like(p)
+
+        for n in range(N):
+            u_left[n] = self.u_hugoniot(p[n], "left")
+        ish = np.where(p > self.left.p)
+        ir = np.where(p < self.left.p)
+
+        plt.plot(p[ish], u_left[ish], c="b", ls=":", lw=2)
+        plt.plot(p[ir], u_left[ir], c="b", ls="-", lw=2)
+        plt.scatter([self.left.p], [self.left.u], marker="x", c="b", s=40)
+
+        for n in range(N):
+            u_right[n] = self.u_hugoniot(p[n], "right")
+        ish = np.where(p > self.right.p)
+        ir = np.where(p < self.right.p)
+
+        plt.plot(p[ish], u_right[ish], c="r", ls=":", lw=2)
+        plt.plot(p[ir], u_right[ir], c="r", ls="-", lw=2)
+        plt.scatter([self.right.p], [self.right.u], marker="x", c="r", s=40)
+
+        du = 0.025*(max(np.max(u_left), np.max(u_right)) -
+                    min(np.min(u_left), np.min(u_right)))
+
+        plt.text(self.left.p, self.left.u+du, "left",
+                 horizontalalignment="center", color="b")
+
+        plt.text(self.right.p, self.right.u+du, "right",
+                 horizontalalignment="center", color="r")
+
+        plt.xlim(p_min, p_max)
+
+        plt.xlabel(r"$p$", fontsize="large")
+        plt.ylabel(r"$u$", fontsize="large")
+
+        legs = []
+        legnames = []
+
+        legs.append(plt.Line2D((0,1),(0,0), color="k", ls=":", marker=None))
+        legnames.append("shock")
+
+        legs.append(plt.Line2D((0,1),(0,0), color="k", ls="-", marker=None))
+        legnames.append("rarefaction")
+
+        plt.legend(legs, legnames, frameon=False, loc="best")
+
+        plt.tight_layout()
+
+        plt.savefig("riemann-phase.pdf")
 
 
+if __name__ == "__main__":
 
-# setup the problem -- Sod
-left = State(p = 1.0, u = 0.0, rho = 1.0)
-right = State(p = 0.1, u = 0.0, rho = 0.125)
+    # setup the problem -- Sod
+    left = State(p = 1.0, u = 0.0, rho = 1.0)
+    right = State(p = 0.1, u = 0.0, rho = 0.125)
 
-# make the plots
-N = 200
+    rp = RiemannProblem(left, right)
 
-p_min = 0.0
-p_max = 1.5
+    rp.plot_hugoniot()
 
-p = np.arange(N)*(p_max - p_min)/N + p_min
+    pstar, ustar = rp.find_star_state()
 
-
-# curves
-u_left, ish, ir = u_hugoniot(p, left, "left")
-
-pylab.plot(p[ish], u_left[ish], c="b", ls=":", lw=2)
-pylab.plot(p[ir], u_left[ir], c="b", ls="-", lw=2)
-pylab.scatter([left.p], [left.u], marker="x", c="b", s=40)
-
-
-u_right, ish, ir = u_hugoniot(p, right, "right")
-
-pylab.plot(p[ish], u_right[ish], c="r", ls=":", lw=2)
-pylab.plot(p[ir], u_right[ir], c="r", ls="-", lw=2)
-pylab.scatter([right.p], [right.u], marker="x", c="r", s=40)
-
-
-du = 0.025*(max(np.max(u_left), np.max(u_right)) - 
-           min(np.min(u_left), np.min(u_right)))
-
-pylab.text(left.p, left.u+du, "left", 
-           horizontalalignment="center", color="b")
-
-pylab.text(right.p, right.u+du, "right", 
-           horizontalalignment="center", color="r")
-
-
-pylab.xlim(p_min, p_max)
-
-pylab.xlabel(r"$p$", fontsize="large")
-pylab.ylabel(r"$u$", fontsize="large")
-
-legs = []
-legnames = []
-
-legs.append(pylab.Line2D((0,1),(0,0), color="k", ls=":", marker=None))
-legnames.append("shock")
-
-legs.append(pylab.Line2D((0,1),(0,0), color="k", ls="-", marker=None))
-legnames.append("rarefaction")
-
-pylab.legend(legs, legnames, frameon=False, loc="best")
-
-pylab.tight_layout()
-
-pylab.savefig("riemann-phase.png")
-pylab.savefig("riemann-phase.eps")
+    print pstar, ustar
