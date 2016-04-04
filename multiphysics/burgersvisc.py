fa1b292f19677b54c1e80266d7e4f64f3ada0e7a
Michael Zingale <michael.zingale@stonybrook.edu>
2016-04-04 11:56:50 -0400

    update style + labels, fix a limiter bug

--- multiphysics/burgersvisc.py
+++ multiphysics/burgersvisc.py
@@ -1,5 +1,5 @@
 """
-solve the viscous Burger's equation: u_t + u u_x = nu u_xx
+solve the viscous Burger's equation: u_t + u u_x = eps u_xx
 
 we use an explicit piecewise linear finite-volume method to evaluate
 the advective flux and then discretize the diffusion part implicitly
@@ -8,16 +8,15 @@ time.
 
 The resulting method is second-order in space and time.
 
-M. Zingale (2013-04-03)
+M. Zingale
 """
 
-import numpy
+import numpy as np
 from scipy import linalg
 import sys
-import pylab
-import math
+import matplotlib.pyplot as plt
 
-class Grid1d:
+class Grid1d(object):
 
     def __init__(self, nx, ng=1, xmin=0.0, xmax=1.0, vars=None):
         """ grid class initialization """
@@ -32,16 +31,16 @@ class Grid1d:
         self.ihi = ng+nx-1
 
         self.dx = (xmax - xmin)/nx
-        self.x = (numpy.arange(nx+2*ng) + 0.5 - ng)*self.dx + xmin
+        self.x = (np.arange(nx+2*ng) + 0.5 - ng)*self.dx + xmin
 
         self.data = {}
 
         for v in vars:
-            self.data[v] = numpy.zeros((2*ng+nx), dtype=numpy.float64)
+            self.data[v] = np.zeros((2*ng+nx), dtype=np.float64)
 
 
     def scratch_array(self):
-        return numpy.zeros((2*self.ng+self.nx), dtype=numpy.float64)
+        return np.zeros((2*self.ng+self.nx), dtype=np.float64)
 
 
     def fill_BCs(self, var):
@@ -56,33 +55,29 @@ class Grid1d:
         vp[self.ihi+1:] = vp[self.ihi]
 
 
-class Simulation:
+class Simulation(object):
 
     def __init__(self, grid):
         self.grid = grid
         self.t = 0.0
 
-
     def init_cond(self):
-        """ initial conditions """
+        """ initial conditions -- a smoothed tophat """
 
         g = self.grid
         u = g.data["u"]
 
-        index = numpy.logical_and(g.x >= 0.333, g.x <= 0.666)
+        index = np.logical_and(g.x >= 0.333, g.x <= 0.666)
         u[:] = 1.0
-        u[index] += 0.5*numpy.sin(2.0*math.pi*(g.x[index]-0.333)/0.333)
-
+        u[index] += 0.5*np.sin(2.0*np.pi*(g.x[index]-0.333)/0.333)
 
     def timestep(self, C):
-        """ estimate the timestep """
+        """ estimate the timestep -- crossing time of a zone """
         u = self.grid.data["u"]
         dt = C*self.grid.dx/max(abs(u[self.grid.ilo:self.grid.ihi+1]))
         return dt
 
-
-
-    def diffuse(self, nu, S, dt):
+    def diffuse(self, eps, S, dt):
         """ diffuse u implicitly (C-N) through timestep dt with source S """
 
         gr = self.grid
@@ -90,20 +85,20 @@ class Simulation:
 
         unew = gr.scratch_array()
     
-        alpha = nu*dt/gr.dx**2
+        alpha = eps*dt/gr.dx**2
 
         # create the RHS of the matrix
-        R = 0.5*nu*dt*self.lap()
+        R = 0.5*eps*dt*self.lap()
         R = R[gr.ilo:gr.ihi+1]
         R += u[gr.ilo:gr.ihi+1]
         R += dt*S[gr.ilo:gr.ihi+1]
     
         # create the diagonal, d+1 and d-1 parts of the matrix
-        d = (1.0 + alpha)*numpy.ones(gr.nx)
-        u = -0.5*alpha*numpy.ones(gr.nx)
+        d = (1.0 + alpha)*np.ones(gr.nx)
+        u = -0.5*alpha*np.ones(gr.nx)
         u[0] = 0.0
 
-        l = -0.5*alpha*numpy.ones(gr.nx)
+        l = -0.5*alpha*np.ones(gr.nx)
         l[gr.nx-1] = 0.0
 
         # set the boundary conditions by changing the matrix elements
@@ -120,11 +115,10 @@ class Simulation:
         #R[gr.nx-1] += alpha*0.0
 
         # solve
-        A = numpy.matrix([u,d,l])
+        A = np.matrix([u,d,l])
         unew[gr.ilo:gr.ihi+1] = linalg.solve_banded((1,1), A, R)
 
         return unew
-
 
     def advect(self, S, dt):
         """ compute the advective term that updates u in time.  Here, S is
@@ -145,14 +139,15 @@ class Simulation:
         dl = gr.scratch_array()
         dr = gr.scratch_array()
 
-        dc[ib:ie+1] = 0.5*numpy.fabs(u[ib+1:ie+2] - u[ib-1:ie ])
-        dl[ib:ie+1] = numpy.fabs(u[ib+1:ie+2] - u[ib  :ie+1])
-        dr[ib:ie+1] = numpy.fabs(u[ib  :ie+1] - u[ib-1:ie ])
+        dc[ib:ie+1] = 0.5*(u[ib+1:ie+2] - u[ib-1:ie ])
+        dl[ib:ie+1] = np.fabs(u[ib+1:ie+2] - u[ib  :ie+1])
+        dr[ib:ie+1] = np.fabs(u[ib  :ie+1] - u[ib-1:ie ])
         
-        minslope = numpy.minimum(dc, numpy.minimum(2.0*dl, 2.0*dr))
-        ldeltau = numpy.where(test > 0.0, minslope, 0.0)*numpy.sign(dc)
+        minslope = np.minimum(np.fabs(dc), np.minimum(2.0*dl, 2.0*dr))
+        ldeltau = np.where(test > 0.0, minslope, 0.0)*np.sign(dc)
 
-        # construct the interface states
+        # construct the interface states, to second order in space and
+        # time
         ul = gr.scratch_array()
         ur = gr.scratch_array()
 
@@ -166,14 +161,14 @@ class Simulation:
 
         # shock speed and shock state
         S = 0.5*(ul + ur)
-        ushock = numpy.where(S > 0.0, ul, ur)
-        ushock = numpy.where(S == 0.0, 0.0, ushock)
+        ushock = np.where(S > 0.0, ul, ur)
+        ushock = np.where(S == 0.0, 0.0, ushock)
 
         # rarefaction solution
-        urare = numpy.where(ur <= 0.0, ur, 0.0)
-        urare = numpy.where(ul >= 0.0, ul, urare)
+        urare = np.where(ur <= 0.0, ur, 0.0)
+        urare = np.where(ul >= 0.0, ul, urare)
         
-        us = numpy.where(ul > ur, ushock, urare)
+        us = np.where(ul > ur, ushock, urare)
 
         # construct the advective update
         F = 0.5*us*us
@@ -182,7 +177,6 @@ class Simulation:
         A[ib:ie+1] = (F[ib:ie+1] - F[ib+1:ie+2])/gr.dx
 
         return A
-
 
     def lap(self):
         """ compute the Laplacian of u, including the first ghost cells """
@@ -199,12 +193,11 @@ class Simulation:
 
         return lapu
 
-
-    def evolve(self, nu, cfl, tmax, dovis=0):
+    def evolve(self, eps, cfl, tmax, dovis=0):
         """ 
         the main evolution loop.  Evolve 
   
-        phi_t + u u_x = nu u_xx
+        phi_t + u u_x = eps u_xx
 
         from t = 0 to tmax
         """
@@ -218,10 +211,10 @@ class Simulation:
 
         # runtime plotting
         if dovis == 1:
-            pylab.ion()
+            plt.ion()
     
         self.t = 0.0
-        while (self.t < tmax):
+        while self.t < tmax:
 
             dt = self.timestep(cfl)
 
@@ -229,13 +222,13 @@ class Simulation:
                 dt = tmax - self.t
 
             # construct the explicit diffusion source
-            S = nu*self.lap()
+            S = eps*self.lap()
 
             # construct the advective update
             A = self.advect(S, dt)
 
             # diffuse for dt
-            unew = self.diffuse(nu, A, dt)
+            unew = self.diffuse(eps, A, dt)
             
             # to just do advection, we'd do this:
             #unew = u + dt*A
@@ -246,12 +239,12 @@ class Simulation:
             self.t += dt
 
             if dovis == 1:
-                pylab.clf()
-                pylab.plot(gr.x, u)
-                pylab.xlim(gr.xmin,gr.xmax)
-                pylab.ylim(0.0,2.0)
-                pylab.title("t = %f" % (t))
-                pylab.draw()
+                plt.clf()
+                plt.plot(gr.x, u)
+                plt.xlim(gr.xmin,gr.xmax)
+                plt.ylim(0.0,2.0)
+                plt.title("t = %f" % (t))
+                plt.draw()
 
 
 nx = 256
@@ -262,36 +255,35 @@ g1 = Grid1d(nx, ng=2, vars=["u"])
 g2 = Grid1d(nx, ng=2, vars=["u"])
 g3 = Grid1d(nx, ng=2, vars=["u"])
 
-nu1 = 0.005
+eps1 = 0.005
 s1 = Simulation(g1)
 s1.init_cond()
-s1.evolve(nu1, cfl, tmax, dovis=0)
+s1.evolve(eps1, cfl, tmax, dovis=0)
 
 
-nu2 = 0.0005
+eps2 = 0.0005
 s2 = Simulation(g2)
 s2.init_cond()
-s2.evolve(nu2, cfl, tmax, dovis=0)
+s2.evolve(eps2, cfl, tmax, dovis=0)
 
 
-nu3 = 0.00005
+eps3 = 0.00005
 s3 = Simulation(g3)
 s3.init_cond()
-s3.evolve(nu3, cfl, tmax, dovis=0)
+s3.evolve(eps3, cfl, tmax, dovis=0)
 
 u1 = s1.grid.data["u"]
-pylab.plot(g1.x, u1, label=r"$\nu = %f$" % (nu1))
+plt.plot(g1.x, u1, label=r"$\epsilon = %f$" % (eps1))
 
 u2 = s2.grid.data["u"]
-pylab.plot(g2.x, u2, label=r"$\nu = %f$" % (nu2), ls="--", color="k")
+plt.plot(g2.x, u2, label=r"$\epsilon = %f$" % (eps2), ls="--", color="k")
 
 u3 = s3.grid.data["u"]
-pylab.plot(g3.x, u3, label=r"$\nu = %f$" % (nu3), ls=":", color="k")
+plt.plot(g3.x, u3, label=r"$\epsilon = %f$" % (eps3), ls=":", color="k")
 
-pylab.legend(frameon=False)
+plt.legend(frameon=False)
 
-pylab.xlim(0.0, 1.0)
+plt.xlim(0.0, 1.0)
 
-pylab.tight_layout()
-pylab.savefig("burgervisc.png")
-pylab.savefig("burgervisc.eps")
+plt.tight_layout()
+plt.savefig("burgersvisc.pdf")
